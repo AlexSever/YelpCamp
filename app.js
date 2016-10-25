00027aa65204880ab3c4424690a1739f59f97072
Aleksei Miasin <alexei.miasin@gmail.com>
2016-10-24 22:23:55 -0400

    V.3 Created models folder to export main modules such as campgrounds and comments. Added seeds.js for working with database. Adding comments to campgroundSchema.

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var express     = require("express"),
 	app         = express(),
 	bodyParser  = require("body-parser"),
-	mongoose    = require("mongoose");
+	mongoose    = require("mongoose"),
+	Campground  = require("./models/campground"),
+	Comment     = require("./models/comment"),
+	//User        = require("./models/user"),
+	seedDB      = require("./seeds")
 
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
+seedDB();
 /*
 Campground.create(    
 	{
@@ -89,10 +86,11 @@ app.get("/campgrounds/new", function(req, res) {
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res) {
 	// find the campground with provided ID
-	Campground.findById(req.params.id, function(err, foundCampground) {
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
 		if (err) {
 			console.log(err);
 		} else {
+			console.log(foundCampground);
 			// render show template with that campground
 			res.render("show", {campground: foundCampground});
 		}
